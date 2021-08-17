29e8a20b549328aa27feeea771a9f88e6c26bcae
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2021-08-17 14:58:00 -0600

    chore: generate python samples templates in owlbot.py

--- owlbot.py
+++ owlbot.py
@@ -29,6 +29,7 @@ s.remove_staging_dirs()
 # Add templated files
 # ----------------------------------------------------------------------------
 templated_files = common.py_library(
+python.py_samples(skip_readmes=True)
     cov_level=98,
     samples=False,  # set to True only if there are samples
     microgenerator=True,
